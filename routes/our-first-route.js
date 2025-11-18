38f052b4e3423b24da7afdf0e76fd45e0a9bd6ae
enerisan <enerisan@gmail.com>
2025-11-18 11:39:56 +0100

    feat(routes): add /animals/name/:animal for get and update

--- routes/our-first-route.js
+++ routes/our-first-route.js
@@ -4,6 +4,8 @@
  * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
  */
 
+import { ObjectId } from "@fastify/mongodb";
+
 async function routes(fastify, options) {
 
     const collection = fastify.mongo.db.collection('test_collection')
@@ -20,19 +22,43 @@ async function routes(fastify, options) {
         return result
     })
 
-    fastify.get('/animals/:animal', async (request, reply) => {
-        const result = await collection.findOne({animal: request.params.animal})
-        if(!result) {
+    fastify.get('/animals/name/:animal', async (request, reply) => {
+        const result = await collection.findOne({ animal: request.params.animal })
+        if (!result) {
             throw new Error('Invalid value')
         }
         return result
     })
 
-    const animalBodyJsonSchema =  {
+    fastify.get('/animals/id/:animalId', async (request, reply) => {
+        const { ObjectId } = fastify.mongo
+        const { animalId } = request.params
+
+        let objectId
+        try {
+            objectId = new ObjectId(animalId)
+        } catch (e) {
+            throw new Error('Invalid value')
+        }
+        const result = await collection.findOne({ _id: objectId })
+
+        if (!result) {
+            throw new Error('Invalid value')
+        }
+
+        return result
+
+    })
+
+
+    const animalBodyJsonSchema = {
         type: 'object',
-        required:['animal'],
-        properties:{
-            animal: {type: 'string'},
+        required: ['animal'],
+        properties: {
+            id: { type: 'integer' },
+            animal: { type: 'string' },
+            speed: { type: 'string' }
+
         }
     }
 
@@ -40,8 +66,32 @@ async function routes(fastify, options) {
         body: animalBodyJsonSchema,
     }
 
-    fastify.post('/animals', {schema}, async (request, reply)=> {
-        const result = await collection.insertOne({animal: request.body.animal})
+    fastify.post('/animals', { schema }, async (request, reply) => {
+        const { animal, speed } = request.body
+        const result = await collection.insertOne({ animal, speed })
+        return { insertedId: result.insertedId }
+    })
+
+    fastify.put('/animals/name/:animal', async (request, reply) => {
+        const oldAnimalName = request.params.animal;
+        const { animal, speed } = request.body;
+        const result = await collection.updateOne(
+            { animal: oldAnimalName },
+            {
+                $set: {
+                    animal,
+                    speed
+                }
+            }
+        )
+
+        if (result.matchedCount === 0) {
+            throw new Error('Animal not found')
+        }
+
+        const updatedAnimal = await collection.findOne({ animal })
+
+        return updatedAnimal;
     })
 }
 
